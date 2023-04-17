2e41f5313a691bc0edc83e7377b4919712f6d4b8
iamlvv <levanvy28062002@gmail.com>
2023-04-17 10:52:45 +0700

    update scan barcode

--- FindingRunningBuddy/App.js
+++ FindingRunningBuddy/App.js
@@ -2,14 +2,46 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Text, View, Image, TouchableOpacity } from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
+import { Camera } from 'expo-camera'
+import React, { useState, useRef, useEffect } from 'react';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+import { Button } from 'react-native';
 const Stack = createNativeStackNavigator();
 
 function HomeScreen(navigation) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View className='items-center'>
-      <Text>This is the HomeScreen</Text>
+    <View>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
-  )
+  );
 }
 
 function OnboardingPage({ navigation }) {
